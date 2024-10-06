d172e1bef62f1751af6794cedcd07590ac8c0d05
Cena <cenababajannjhad@gmail.com>
2024-10-06 14:03:51 +0330

    Add files via upload

---
+++ passGenerator/script.js
@@ -0,0 +1,12 @@
+const passwordGenerator = (length) => {
+  const chars ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  return password;
+};
+
+console.log(passwordGenerator(54));
